60774f2e1f1cf3117446f8071ae8212e97b1382d
Myron Ladyjenko <ladyjenkomyron@gmail.com>
2024-12-01 23:38:23 +0000

    Bilateral filter

--- a3.py
+++ a3.py
@@ -14,10 +14,12 @@ from PIL import Image
 def gaussian_matrix_value(
     i: wp.int32, j: wp.int32, sigma: wp.float32, kernel_size: wp.int32
 ):
-    x = wp.float32(i - kernel_size // 2)
-    y = wp.float32(j - kernel_size // 2)
+    # x = wp.float32(i - kernel_size // 2)
+    # y = wp.float32(j - kernel_size // 2)
+    x = wp.float32(i)
+    y = wp.float32(j)
 
-    return wp.exp(-(x * x + y * y) / (2.0 * sigma * sigma))
+    return wp.exp(-(x * x + y * y) / (2.0 * sigma * sigma)) * (1.0 / (2.0 * sigma * sigma * np.pi))
 
 
 @wp.kernel
@@ -28,20 +30,28 @@ def gaussian_blur_kernel(
     param: wp.float32,
 ):
     gaussian_matrix_norm = float(0.0)
-    for i in range(kernel_size):
-        for j in range(kernel_size):
+    loop_range = int(3.0 * param)
+
+    # for i in range(kernel_size):
+    #     for j in range(kernel_size):
+    for i in range(-loop_range, loop_range + 1):
+        for j in range(-loop_range, loop_range + 1):
             gaussian_matrix_norm += gaussian_matrix_value(i, j, param, kernel_size)
 
     width, height = input.shape[0], input.shape[1]
     x, y, k = wp.tid()
 
     # Apply the kernel to the input pixel
-    for i in range(kernel_size):
-        for j in range(kernel_size):
+    # for i in range(kernel_size):
+    #     for j in range(kernel_size):
+    for i in range(-loop_range, loop_range + 1):
+        for j in range(-loop_range, loop_range + 1):
             # Edge handling strategy: "extend"
             # https://en.wikipedia.org/wiki/Kernel_(image_processing)#Edge_handling
-            xi = wp.clamp(x + i - kernel_size // 2, 0, width - 1)
-            yi = wp.clamp(y + j - kernel_size // 2, 0, height - 1)
+            # xi = wp.clamp(x + i - kernel_size // 2, 0, width - 1)
+            # yi = wp.clamp(y + j - kernel_size // 2, 0, height - 1)
+            xi = wp.clamp(x + i, 0, width - 1)
+            yi = wp.clamp(y + j, 0, height - 1)
 
             matrix_val = (
                 gaussian_matrix_value(i, j, param, kernel_size) / gaussian_matrix_norm
@@ -89,10 +99,57 @@ def unsharp_masking_kernel(
 
             output[x, y, k] += matrix_val * input[xi, yi, k]
 
-    # Ensure pixel values are in the range [0, 255]
     output[x, y, k] = wp.clamp(output[x, y, k], 0.0, 255.0)
 
 
+@wp.func
+def bilateral_filter_matrix_value(
+    i: wp.int32, j: wp.int32, sigma_spatial: wp.float32, sigma_intensity: wp.float32, input: wp.array3d(dtype=wp.float32), 
+    x: wp.int32, y: wp.int32, k: wp.int32, kernel_size: wp.int32, width: wp.int32, height: wp.int32
+):
+    xj = wp.float32(i - kernel_size // 2)
+    yj = wp.float32(j - kernel_size // 2)
+    spatial_weight = wp.exp(-(xj * xj + yj * yj) / (2.0 * sigma_spatial * sigma_spatial)) * (1.0 / (2.0 * sigma_spatial * sigma_spatial * np.pi))
+
+    xi = wp.clamp(x + i - kernel_size // 2, 0, width - 1)
+    yi = wp.clamp(y + j - kernel_size // 2, 0, height - 1)
+    intensity_weight = wp.exp(-wp.abs(input[x, y, k] - input[xi, yi, k]) / (2.0 * sigma_intensity * sigma_intensity)) * (1.0 / (2.0 * sigma_intensity * sigma_intensity * np.pi))
+
+    return spatial_weight * intensity_weight
+
+
+@wp.kernel
+def bilateral_filter_kernel(
+    input: wp.array3d(dtype=wp.float32),
+    output: wp.array3d(dtype=wp.float32),
+    kernel_size: wp.int32,
+    sigma_spatial: wp.float32,
+    sigma_intensity: wp.float32,
+):
+    width, height = input.shape[0], input.shape[1]
+    x, y, k = wp.tid()
+
+    bilateral_matrix_norm = float(0.0)
+    for i in range(kernel_size):
+        for j in range(kernel_size):
+            bilateral_matrix_norm += bilateral_filter_matrix_value(i, j, sigma_spatial, sigma_intensity, input, x, y, k, kernel_size, width, height)
+    if (x == 0 and y == 0):
+        print(bilateral_matrix_norm)
+
+    for i in range(kernel_size):
+        for j in range(kernel_size):
+            # Handle edges
+            xi = wp.clamp(x + i - kernel_size // 2, 0, width - 1)
+            yi = wp.clamp(y + j - kernel_size // 2, 0, height - 1)
+
+            matrix_val = (
+                bilateral_filter_matrix_value(i, j, sigma_spatial, sigma_intensity, input, x, y, k, kernel_size, width, height) / bilateral_matrix_norm
+            )
+            output[x, y, k] += matrix_val * input[xi, yi, k]
+
+    output[x, y, k] = wp.clamp(output[x, y, k], 0.0, 255.0)
+
+# START
 # Basic argument checking
 if len(sys.argv) < 6:
     print(
@@ -110,9 +167,23 @@ device = "cpu"
 # Parse command line arguments
 alg_type = sys.argv[1]
 kernel_size = int(sys.argv[2])
-param = float(sys.argv[3])
-input_filename = sys.argv[4]
-output_filename = sys.argv[5]
+
+spatial_param = None
+intensity_param = None
+param = None
+input_filename = None
+output_filename = None
+if alg_type == "-b":
+    spatial_param = float(sys.argv[3])
+    intensity_param = float(sys.argv[4])
+    input_filename = sys.argv[5]
+    output_filename = sys.argv[6]
+else:
+    param = float(sys.argv[3])
+    input_filename = sys.argv[4]
+    output_filename = sys.argv[5]
+
+print(spatial_param, intensity_param)
 
 # Validate command line arguments
 assert kernel_size >= 0, "Kernel size must positive"
@@ -123,6 +194,10 @@ if alg_type == "-s":
 elif alg_type == "-n":
     assert param > 0.0, "Parameter must be positive for gaussian blur"
     kernel = gaussian_blur_kernel
+elif alg_type == "-b":
+    assert spatial_param > 0.0, "Parameter must be positive for bilateral filter"
+    assert intensity_param > 0.0, "Parameter must be positive for bilateral filter"
+    kernel = bilateral_filter_kernel
 else:
     raise ValueError(f"Invalid algorithm type: {alg_type}")
 
@@ -138,12 +213,21 @@ if input_image.mode == "L":
 input_wp = wp.array(input, dtype=wp.float32, device=device)
 output_wp = wp.zeros(input_wp.shape, dtype=wp.float32, device=device)
 
-wp.launch(
-    kernel=kernel,
-    dim=input_wp.shape,
-    inputs=[input_wp, output_wp, kernel_size, param],
-    device=device,
-)
+if alg_type == "-b":
+    print(kernel_size, spatial_param, intensity_param, kernel)
+    wp.launch(
+        kernel=kernel,
+        dim=input_wp.shape,
+        inputs=[input_wp, output_wp, kernel_size, spatial_param, intensity_param],
+        device=device,
+    )
+else:
+    wp.launch(
+        kernel=kernel,
+        dim=input_wp.shape,
+        inputs=[input_wp, output_wp, kernel_size, param],
+        device=device,
+    )
 
 output = np.uint8(output_wp)
 
